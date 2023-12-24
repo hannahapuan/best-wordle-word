5ab1919d1815ca39efcb88df7bc2fd0540460b9b
Hannah Apuan <hapuan@twilio.com>
2023-12-23 18:02:00 -0700

    cleaning up code

--- main.go
+++ main.go
@@ -11,10 +11,23 @@ import (
 
 func main() {
 	// create cardinality map of each occurrence of a letter in each position
-	cardMap := make(map[string]map[int]int)
+	cardMap := initalizeCardMap(ref.WordleList)
 
-	// initialize map of each letter's cardinality in each position
+	// create map of each letter's total cardinality and fill it with each word's score
+	wordScores := make(map[string]int)
 	for _, word := range ref.WordleList {
+		wordScores[word] = determineWordScore(word, cardMap)
+	}
+
+	// sort into a priority queue where score is the priority
+	ws := sortWordScores(wordScores)
+	ws.Print()
+}
+
+// initalizeCardMap creates a map of each letter's cardinality in each position
+func initalizeCardMap(wordList []string) map[string]map[int]int {
+	cardMap := make(map[string]map[int]int)
+	for _, word := range wordList {
 		for i, letter := range word {
 			l := string(letter)
 			if _, ok := cardMap[l]; !ok {
@@ -24,21 +37,21 @@ func main() {
 		}
 	}
 
-	// create map of each letter's total cardinality
-	wordScores := make(map[string]int)
-	for _, word := range ref.WordleList {
-		var score int
-		for j, letter := range word {
-			l := string(letter)
-			score += cardMap[l][j]
-		}
-		wordScores[word] = score
-	}
-
-	ws := sortWordScores(wordScores)
-	ws.Print()
+	return cardMap
 }
 
+// determineWordScore determines the score by adding the cardinality of each letter in each position
+func determineWordScore(word string, cardMap map[string]map[int]int) int {
+	var score int
+	for j, letter := range word {
+		l := string(letter)
+		score += cardMap[l][j]
+	}
+
+	return score
+}
+
+// sortWordScores sorts the word scores into a priority queue
 func sortWordScores(wordScores map[string]int) ref.PriorityQueue {
 	sortedWords := make(ref.PriorityQueue, 0)
 
